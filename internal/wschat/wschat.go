40ed225d876ec1a2658863ed939812f981865878
Antonio Felguerez <antoniof@spotify.com>
2023-11-10 17:40:16 -0500

    Add a comment

--- internal/wschat/wschat.go
+++ internal/wschat/wschat.go
@@ -15,7 +15,7 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
-		//fmt.Printf("origin: %s\n", r.Header.Get("Origin"))
+		// @TODO: secure this function
 		return true
 	},
 }
