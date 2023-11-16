3c75bb9621250f5034f12d5a8dc0c6997ad015f7
Antonio Felguerez <antoniof@spotify.com>
2023-11-16 09:39:25 -0500

    Use labels in strings, rm task files

--- scripts/send.sh
+++ scripts/send.sh
@@ -16,33 +16,34 @@ fi
 SERVICE_TYPE=$1
 PROJECT_ROOT_DIR="$(dirname "$0")/.."  # Assumes the script is in a subdirectory of the project
 cd $PROJECT_ROOT_DIR
+SERVICE_LABEL="grpchat-${SERVICE_TYPE}"
 
 # Step 1: Build and Push Docker Image
-print_message "34" "🚀 Starting build and push for $SERVICE_TYPE..."
+print_message "34" "🚀 Starting build and push for $SERVICE_LABEL..."
 ./scripts/build.sh $SERVICE_TYPE
 if [ $? -ne 0 ]; then
-    print_message "31" "❌ Build and push for $SERVICE_TYPE failed."
+    print_message "31" "❌ Build and push for $SERVICE_SERVICE_LABEL failed."
     exit 1
 fi
-print_message "32" "✅ Build and push for $SERVICE_TYPE succeeded."
+print_message "32" "✅ Build and push for $SERVICE_SERVICE_LABEL succeeded."
 
 # Step 2: Update ECS Task Definition with the new image tag
-print_message "34" "🚀 Updating ECS task definition for $SERVICE_TYPE..."
+print_message "34" "🚀 Updating ECS task definition for $SERVICE_LABEL..."
 ./scripts/update_ecs_task.sh $SERVICE_TYPE
 if [ $? -ne 0 ]; then
     print_message "31" "❌ Update ECS task definition for $SERVICE_TYPE failed."
     exit 1
 fi
-print_message "32" "✅ ECS task definition for $SERVICE_TYPE updated."
+print_message "32" "✅ ECS task definition for $SERVICE_LABEL} updated."
 
 # Step 3: Deploy the new version to ECS
-print_message "34" "🚀 Initiating deployment for $SERVICE_TYPE..."
+print_message "34" "🚀 Initiating deployment for $SERVICE_LABEL..."
 ./scripts/deploy.sh $SERVICE_TYPE
 if [ $? -ne 0 ]; then
-    print_message "31" "❌ Deployment for $SERVICE_TYPE failed."
+    print_message "31" "❌ Deployment for $SERVICE_LABEL failed."
     exit 1
 fi
-print_message "32" "✅ Deployment for $SERVICE_TYPE initiated."
+print_message "32" "✅ Deployment for $SERVICE_LABEL initiated."
 
-print_message "35" "🎉 $SERVICE_TYPE service has been built, task definition updated, and deployment initiated."
+print_message "35" "🎉 $SERVICE_LABEL service has been built, task definition updated, and deployment initiated."
 print_message "35" "🎉 Check the ECS console for the status of the deployment."
